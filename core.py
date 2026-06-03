4cfe43b2192ac7a8f87ce166d9858bc429859a2f
Jenil05-web <jeniljoshi56@gmail.com>
2026-06-03 13:14:21 +0530

    core.py file updated

--- core.py
+++ core.py
@@ -36,8 +36,8 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_community.vectorstores import FAISS
-from langchain_classic.chains import create_retrieval_chain
-from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
 from langsmith import traceable
 from langgraph.graph import StateGraph, END
 
